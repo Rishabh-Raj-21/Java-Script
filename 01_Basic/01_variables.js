53fb0a3a9371c4af2dd6214fc6e59384152de852
Rishabh Raj <120393425+Rishabh-Raj-21@users.noreply.github.com>
2024-05-26 20:31:43 +0000

    datatypes

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,10 @@
+const account_Id = 1269
+let accountEmail = "rishabh@gmail.com"
+var accountPassword = "@1234"
+accountCity = "Gaya"
+
+// account_Id = 2
+console.log(account_Id);
+console.table([account_Id,accountEmail,accountPassword,accountCity])
+
+/* avoid using var because of issue of block and functional scope */
---
+++ 01_Basic/02_variables.js
@@ -0,0 +1,8 @@
+"use strict" // use all JS code as a newer version
+
+// null => Standalone value  (typeof null is an object)
+// undefined => value not assigned
+// symbol => it defines unique
+
+console.log(typeof null);
+console.log(typeof undefined);
