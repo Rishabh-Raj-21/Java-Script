2e1438022c97595c722d4254fd0dedb04eaecbd7
Rishabh Raj <120393425+Rishabh-Raj-21@users.noreply.github.com>
2024-06-04 21:19:07 +0530

    Update 02_variables.js

--- 01_Basic/02_variables.js
+++ 01_Basic/02_variables.js
@@ -1,7 +1,7 @@
 "use strict" // use all JS code as a newer version
 
-// null => Standalone value  (typeof null is an object)
-// undefined => value not assigned
+// null => Standalone value  (typeof null is an object)   Null is an assignment value, meaning that a variable has been declared and given the value of null
+// undefined => variable has been declared but has not yet been assigned a value
 // symbol => it defines unique
 
 console.log(typeof null);
