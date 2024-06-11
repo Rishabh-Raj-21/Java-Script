a5a24e401e73020470bfcbafdf7b1aee0eb61b80
Rishabh Raj <120393425+Rishabh-Raj-21@users.noreply.github.com>
2024-06-11 12:33:11 +0530

    Update Coversion_Operation.js

--- 01_Basic/Coversion_Operation.js
+++ 01_Basic/Coversion_Operation.js
@@ -1,1 +1,11 @@
+let score = "33abc"
+// console.log(typeof score);
 
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+//  Output depends upon input type 
+//  score = "abc" => Output = Not a number
+// JS treats null as a 0 when operators are >, >=, <, <=
