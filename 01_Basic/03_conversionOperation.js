46bb4aec28e8e40da03870c13c4985f6f386b07c
Rishabh Raj <120393425+Rishabh-Raj-21@users.noreply.github.com>
2024-06-11 12:18:18 +0530

    Update 03_conversionOperation.js
    type conversion of number and boolean

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -1,2 +1,21 @@
-let score = 33
+let score = "33"
 console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(valueInNumber);          // It will return Not a Number, it is a drawback of js
+console.log(typeof valueInNumber);  // Number
+
+// Output values are dependend upon Input values
+//  Type conversion of Number(score)
+// Input       Output
+// "String"     Nan
+//  null        0
+//  undefined   Nan
+// true         1
+// false        0
+
+// Type conversion of Boolean(score)
+// Input         Output
+// 1              true
+// 0              false
+// ""(empty str)  false
+// "abc"          true
